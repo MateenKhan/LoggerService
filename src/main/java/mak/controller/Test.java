ac7cffbe35d3e37b2eae6f276c36c70d5f839177
Mateen <makjavaprogrammer@gmail.com>
2022-11-27 21:16:24 +0530

    added postman collection and removed unused references

--- src/main/java/mak/controller/Test.java
+++ src/main/java/mak/controller/Test.java
@@ -1,20 +1,23 @@
 package mak.controller;
 
 import mak.model.Employee;
+import mak.service.TestService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import mak.service.TestService;
 
 @RestController
 public class Test {
 
     TestService service;
-    Test(TestService service){
+
+    Test(TestService service) {
         this.service = service;
     }
 
     @PostMapping
-    public Employee employee(@RequestBody Employee employee){return service.asdf(employee);}
+    public Employee employee(@RequestBody Employee employee) {
+        return service.asdf(employee);
+    }
 
 }
